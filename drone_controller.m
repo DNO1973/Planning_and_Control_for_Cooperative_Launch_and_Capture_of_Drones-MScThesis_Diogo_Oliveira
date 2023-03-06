3fc5cd81e453de89c33e7970923a8235581b7801
dnuoliveira <115311366+dnuoliveira@users.noreply.github.com>
2023-03-06 18:00:45 +0000

    Delete drone_controller.m

--- drone_controller.m
+++
@@ -1,9 +0,0 @@
-function u = drone_controller(p,v,p_ref,P)
-%DRONE_CONTROLLER Summary of this function goes here
-%   Detailed explanation goes here
-    
-    u = -diag([P.kp/2,P.kp/2,P.kp])*(p-p_ref) + ...
-        -diag([P.kv/2,P.kv/2,P.kv])*v + [0;0;P.m*P.g];
-    
-end
-

d075ff621af6021dcb1aaed65af91ec4482bf8b4
dnuoliveira <115311366+dnuoliveira@users.noreply.github.com>
2023-10-23 15:47:47 +0100

    Add files via upload

---
+++ mpc_simulation/mpc_controller.m
@@ -0,0 +1,29 @@
+function [control_action,x_seq_next,u_seq_next] = mpc_controller(current_state,P,x_seq_prev,u_seq_prev, mpc_external_function)
+
+        use_external_function = 0;
+        
+        if ~use_external_function
+
+            P.MPC.set_value(P.xx0,current_state);
+            P.MPC.set_initial(P.xx,x_seq_prev);
+            P.MPC.set_initial(P.uu,u_seq_prev);
+            sol = P.MPC.solve();
+            x_seq = sol.value(P.xx);
+            u_seq = sol.value(P.uu);
+            x_seq_next = [x_seq(:,2:end),x_seq(:,end)];
+            u_seq_next = [u_seq(:,2:end),u_seq(:,end)];
+            %control_action = P.m*u_seq(1:3,1) + P.m*P.g*zW;
+            control_action = [P.m*u_seq(1:3,1) - P.m*P.g*[0;0;1];u_seq(4,1)];
+            %control_action = u_seq;
+        
+        else
+            
+            x_seq_next = x_seq_prev;
+            u_seq_next = u_seq_prev;
+            
+            [u_seq, x_seq] = mpc_external_function(current_state);
+            control_action = [P.m*u_seq(1:3,1) - P.m*P.g*[0;0;1];u_seq(4,1)];
+        
+        end
+end
+
---
+++ mpc_simulation/mpc_simulation_shuttle_and_target.m
@@ -0,0 +1,232 @@
+
+clear all; clc; close all;
+
+
+addpath('D:\Docments\Matlab\toolbox\casadi-3.6.3-windows64-matlab2018b')
+import casadi.*
+
+
+set_param_fixed_complex;
+
+
+mpc_setup;
+mpc_external_function = Function.load('F.casadi');
+
+Ts =Param.T/Param.N;
+Param.m = 4;
+Param.g = 9.81;
+
+%% Defining the system of Target ------------------------------------------------------------------------------------------
+
+% Constrains of UAV
+
+%input constrains
+v_target_min = 10; v_target_max = 20;
+psi_target_min = -inf; psi_target_max = inf;
+  
+%states constrains
+
+gamma_target_min = -pi/6; gamma_target_max = pi/6;
+phi_target_min = -pi/6; phi_target_max = pi/6;
+
+% States of UAV 
+
+x_target = SX.sym('x_target'); y_target = SX.sym('y_target'); z_target = SX.sym('z_target'); psi_target = SX.sym('psi_target');   %states of the UAV
+states_target = [x_target; y_target; z_target; psi_target];      n_states_target = length(states_target);               %UAV
+
+% Controls of UAV 
+
+v_target = SX.sym('v_target'); gamma_target = SX.sym('gamma_target'); phi_target = SX.sym('phi_target');         % UAV cotrols parameters
+
+controls_target = [v_target; gamma_target; phi_target]; n_controls_target = length(controls_target);
+rhs_target = [v_target*cos(psi_target)*cos(gamma_target); v_target*sin(psi_target)*cos(gamma_target); -v_target*sin(gamma_target); (9.81/v_target)*tan(phi_target)];                                                        %systems r.h.s
+
+%possivelmente nao vai ser usado e depois é na funcao shitf que se faz o controlo do target
+f_target = Function('f_target',{states_target,controls_target}, {rhs_target});                                         % Nonlinear Mapping Function f(x,u)
+
+
+%% Defining the system of Shuttle ------------------------------------------------------------------------------------------
+
+% Constrains of UAV
+
+%input constrains
+%v_target_min = 10; v_target_max = 20;
+%psi_target_min = -inf; psi_target_max = inf;
+  
+%states constrains
+
+%gamma_target_min = -pi/6; gamma_target_max = pi/6;
+%phi_target_min = -pi/6; phi_target_max = pi/6;
+
+% States of UAV 
+
+
+
+
+p_shuttle = MX.sym('p_shuttle',3); v_shuttle = MX.sym('v_shuttle',3); psi_shuttle = MX.sym('psi_shuttle',1);
+states_shuttle = [p_shuttle; v_shuttle; psi_shuttle];      n_states_shuttle = size(states_shuttle,1);               %UAV
+
+% Controls of UAV 
+a_shuttle = MX.sym('a_shuttle',3); omegaz_shuttle = MX.sym('omegaz_shuttle',1);
+controls_shuttle = [a_shuttle;omegaz_shuttle];                      n_controls_shuttle = size(controls_shuttle,1);
+
+
+rhs_shuttle = [v_shuttle;1/Param.m*a_shuttle+[0;0;1]*Param.g;omegaz_shuttle];                                                        %systems r.h.s
+
+
+f_shuttle = Function('f_shuttle',{states_shuttle,controls_shuttle}, {rhs_shuttle});                                         % Nonlinear Mapping Function f(x,u)
+
+
+
+
+
+
+
+%% Simulation starts from here----------------------------------------------------------------------------------------------
+t0 = 0;
+xt = [ParamFixComplex.p0;ParamFixComplex.psi0]; %initial location of target drone
+
+p_shuttle =[200; 10; -15];
+v_shuttle =[0;0;0];
+psi_shuttle = 0;
+
+
+current_path_segments = [];
+mpc_time_delay = [];
+
+target_reached_inform_point = 0;
+inform_point = [50;20;-10];
+
+
+t(1) = t0;
+sim_time = 200;
+
+% NMPC starts form here
+
+k = 1; %k = mpciter
+                                
+target_states = [];
+target_states(:,1) = xt;
+
+shuttle_states = []; %states in mpc, with shuttle and target dynamics
+shuttle_states(:,1) = [p_shuttle;v_shuttle;psi_shuttle;[xt(1);xt(2);xt(3)]; [0;0;0]; xt(4)];
+%shuttle_states(:,1) = [p_shuttle(1),p_shuttle(2),p_shuttle(3),v_shuttle(1),v_shuttle(2),v_shuttle(3),psi_shuttle,xt(1),xt(2),xt(3), 0;0;0, xt(4)];
+
+main_loop = tic;
+while (k < sim_time/Ts +1)
+    
+    
+    
+    
+    
+    if target_reached_inform_point
+        mpc_tic = tic;
+        [u_shuttle,x_MPC,u_MPC] = mpc_controller(shuttle_states(:,k),Param,x_MPC,u_MPC, mpc_external_function);
+        %u_shuttle = 0;
+        mpc_toc = toc(mpc_tic);
+        mpc_time_delay= horzcat(mpc_time_delay, mpc_toc);
+        
+    else
+        u_shuttle = [0;0; - Param.m*Param.g;0];
+    end
+    t(k) = t0;
+    
+    [path_segment, path_type]  = complex_path_manager(target_states(1:3,k),  ParamFixComplex, current_path_segments);
+    
+    [t0, xt, xs] = apply_controls(Ts, t0, f_target, xt,f_shuttle,shuttle_states(:,k),u_shuttle(:,1), ParamFixComplex, path_segment, path_type); % get the initialization of the next optimization step
+                                                                          %apply control action to the system
+                                                                        %discretizacao/avancar o tempo
+    target_states(:,k+1) = xt;
+    shuttle_states(:,k+1) = [xs;xt(1:3,:);(xt(1:3,:)-shuttle_states(8:10,k))/Ts;xs(4,:)];
+    
+    
+
+    
+    if  (sqrt((inform_point(1) - xt(1))^2 + (inform_point(2) - xt(2))^2)) < 5 
+        target_reached_inform_point = 1;
+    end
+    %k,t0
+    k = k + 1;
+
+end
+main_loop_time = toc(main_loop)
+
+mpc_average_computation_time = mean(mpc_time_delay);
+mpc_average_computation_time
+%% Plotting stuff----------------------------------------------------------------------------------------------------------------------
+ figure(1);
+plot3(target_states(1,:),target_states(2,:), target_states(3,:),shuttle_states(1,:),shuttle_states(2,:),shuttle_states(3,:));
+%plot3(target_states(1,:),target_states(2,:), target_states(3,:));
+hold on;
+title('Target and Shuttle Positions');
+%plot3(target_states(1,1:50:end),target_states(2,1:50:end), target_states(3,1:50:end),'o','Color',"b");
+%plot3(shuttle_states(1,1:50:end),shuttle_states(2,1:50:end), shuttle_states(3,1:50:end),'o','Color',"r");
+xlabel('X[m]'); ylabel('y[m]'); zlabel('z[m]');
+set(gca, 'Zdir', 'reverse');
+legend('Target', 'Shuttle');
+grid on;
+axis equal;
+axis([-50 500 -50 100 -50 50]);
+  
+hold off;
+
+
+
+
+     figure(2);
+     plot(1:sim_time/Ts + 1, shuttle_states(1:3,:), '-');
+    
+     hold on;
+     title('Shuttle and Target Positions');
+      
+     plot(1:sim_time/Ts + 1, target_states(1:3,:), '--');
+     legend('x_{shuttle}','y_{shuttle}','z_{shuttle}','x_{target}','y_{target}','z_{target}','Interpreter','tex');  
+    
+ hold off;
+ 
+ hold off;
+     figure(3);
+     plot(1:sim_time/Ts + 1, shuttle_states(4,:), '-');
+     hold on;
+     title('Shuttle and Target Yaw');
+     plot(1:sim_time/Ts + 1, target_states(4,:), '--');
+     legend('psi_{shuttle}','psi_{target}');  
+    
+
+ hold off;
+ 
+ hold off;
+     figure(4);
+     plot(1:sim_time/Ts + 1, shuttle_states(4:6,:), '-');
+     hold on;
+     title('Shuttle Velocity ');
+     legend('v_x','v_y','v_z');
+    
+    
+
+hold off;
+
+  figure(5);
+  
+    error = vecnorm(shuttle_states(1:3,:) - target_states(1:3,:));
+     plot(1:sim_time/Ts + 1, error, '-');
+    
+     hold on;
+     title('ponto de contacto');
+      
+     %plot(1:sim_time/T + 1, target_states(1:3,:), '--');
+     %legend('x_{shuttle}','y_{shuttle}','z_{shuttle}','x_{target}','y_{target}','z_{target}','Interpreter','tex');  
+    
+ hold off;
+%   figure(6);
+%   
+%     %error = vecnorm(shuttle_states(1:3,:) - target_states(1:3,:));
+%      plot(1:sim_time/Ts , mpc_time_delay);
+%     
+%      hold on;
+%      title('Shasditions');
+%       
+%      %plot(1:sim_time/T + 1, target_states(1:3,:), '--');
+%      %legend('x_{shuttle}','y_{shuttle}','z_{shuttle}','x_{target}','y_{target}','z_{target}','Interpreter','tex');  
+%     
+%  hold off;
